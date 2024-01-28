dbd4f3792cb41cad22a1885415d09cba61755c9d
Drachir000 <mra.berginski@gmail.com>
2024-01-28 12:57:55 +0100

    Initial Commit
     * Add README, LICENSE, pom.xml, and .gitignore files
    
    This commit includes the introduction of four initial files for the project. A README.md file provides a brief description of the SimpleConfigLib project. The LICENSE file includes the MIT License details for the project. An initial pom.xml is added for managing the project's build, while a .gitignore file defines the files/directories to ignore within the repository. (~AI)
    
    Signed-off-by: Drachir000 <mra.berginski@gmail.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>SimpleConfigLib</h1>
+A Simple Library for the handling of JSON Configurations.
+<h2>Work in Progress!</h2>
---
+++ src/test/java/de/drachir000/utils/config/MainTest.java
@@ -0,0 +1,12 @@
+package de.drachir000.utils.config;
+
+import org.junit.Test;
+
+public class MainTest {
+	
+	@Test
+	public void initTest() {
+	
+	}
+	
+}
