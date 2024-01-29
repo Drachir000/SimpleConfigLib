6c0ae21f9d36c57997c4e1ac62a876e924fa598e
Drachir000 <mra.berginski@gmail.com>
2024-01-29 09:50:29 +0100

    Add configuration creation methods to SimpleConfigLib
     * Edit SimpleConfigLib.java
    
    Two new static methods have been added to the SimpleConfigLib class for configuration creation. An emptyConfiguration() method returns an empty Configuration object. The configurationFromString() method creates a Configuration object from a supplied JSON string. (~AI)
    
    Signed-off-by: Drachir000 <mra.berginski@gmail.com>

--- src/main/java/de/drachir000/utils/config/SimpleConfigLib.java
+++ src/main/java/de/drachir000/utils/config/SimpleConfigLib.java
@@ -1,8 +1,21 @@
 package de.drachir000.utils.config;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
+/**
+ * SimpleConfigLib is a class that provides static utility methods for working with configurations.
+ */
 public class SimpleConfigLib {
+	
+	/**
+	 * Creates an empty configuration.
+	 *
+	 * @return an empty {@link Configuration} object
+	 */
+	public static Configuration emptyConfiguration() {
+		return new Configuration(new JSONObject());
+	}
 	
 	/**
 	 * Constructs a {@link Configuration} object from a {@link JSONObject}.
@@ -13,6 +26,17 @@ public class SimpleConfigLib {
 	 */
 	public static Configuration configurationFromJSONObject(JSONObject jsonObject) {
 		return new Configuration(jsonObject);
+	}
+	
+	/**
+	 * Constructs a {@link Configuration} object from a JSON string.
+	 *
+	 * @param source the JSON string representing the configuration
+	 * @return a {@link Configuration} object created from the JSON string
+	 * @throws JSONException If there is a syntax error in the source string or a duplicated key.
+	 */
+	public static Configuration configurationFromString(String source) throws JSONException {
+		return new Configuration(new JSONObject(source));
 	}
 	
 }
